6003567c7eefd5135a8e163dfb901b45c2e4868e
jovo <joshuav@jhu.edu>
2009-06-16 18:21:35 -0400

    made wrapper.m just for vincent

--- code/scripts/spatial_multi.m
+++ code/scripts/spatial_multi.m
@@ -97,7 +97,7 @@ Est.w       = width;                        % width of frame (pixels)
 
 % infer spike trains using a variety of techniques
 q=0;
-exps=[1 2 2.5];% 3 3.5];
+exps=[1];% 3 3.5];
 for qq=exps
     disp(qq)
     if qq==1;                                % use true params
