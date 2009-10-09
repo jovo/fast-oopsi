2a4e47fb565cea611c9e2bd194f2e6b8981a162a
jovo <joshuav@jhu.edu>
2009-10-09 14:02:21 -0400

    removed /old/ and some data that is not used, to make repo smaller

--- old/old_code/GetLik.m
+++
@@ -1,5 +0,0 @@
-function l = GetLik(F,C,n,T,dt,P)
-%     C   = filter(1,[1 -P.gam],n);               % calcium concentration
-    l   = 0.5*T*log(2*pi*P.sig^2) + norm(F-P.a*C-P.b)^2/(2*P.sig^2)...
-        - T*log(P.lam*dt) + P.lam*dt*sum(n);
-end
