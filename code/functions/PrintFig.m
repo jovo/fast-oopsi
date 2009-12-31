c9955f538103a53cacbff87e522dc7f188cb4e9b
jovo <joshuav@jhu.edu>
2009-12-31 16:25:17 -0500

    not sure if anything changed other than some reorganization

--- code/functions/PrintFig.m
+++
@@ -1,8 +0,0 @@
-function PrintFig(wh,DirName,FileName)
-
-set(gcf,'PaperSize',wh,'PaperPosition',[0 0 wh],'Color','w');
-FigName = [DirName FileName];
-print('-depsc',FigName)
-print('-dpdf',FigName)
-
-end
