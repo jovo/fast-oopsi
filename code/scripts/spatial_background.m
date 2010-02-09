924f430bca7cf419bb014aecb8f96abef847d95a
jovo <joshuav@jhu.edu>
2010-02-09 17:34:13 -0500

    spatial inference works (but not learning)

--- code/scripts/spatial_background.m
+++ code/scripts/spatial_background.m
@@ -13,7 +13,7 @@ clear, clc
 
 % 1) generate spatial filters
 
-% % stuff required for each spatial filter
+% stuff required for each spatial filter
 Nc      = 1;                        % # of cells in the ROI
 neur_w  = 15;                       % width per neuron
 width   = 15;                       % width of frame (pixels)
@@ -21,48 +21,46 @@ height  = Nc*neur_w;                % height of frame (pixels)
 Npixs   = width*height;             % # pixels in ROI
 x       = linspace(-5,5,height);
 y       = linspace(-5,5,width);
-[X,Y]   = meshgrid(x,y);            
+[X,Y]   = meshgrid(x,y);
 g1      = zeros(Npixs,Nc);
 g2      = 0*g1;
 Sigma1  = diag([1,1])*2;            % var of positive gaussian
 Sigma2  = diag([1,1])*2.5;          % var of negative gaussian
 mu      = [0 0];                    % means of gaussians for each cell (distributed across pixel space)
-w       = 1;                        % weights of each filter
+w       = [1 0.5];                  % weights of each filter
 
 
 ix0 = [97:99 112:114 127:129];      % typical spatial filter
 for i=1:Nc                          % spatial filter
     g1(:,i)  = w(i)*mvnpdf([X(:) Y(:)],mu(:,i)',Sigma1);
-    g2(:,i)  = 0*w(i)*mvnpdf([X(:) Y(:)],mu(:,i)',Sigma2);
+    g2(:,i)  = w(i)*mvnpdf([X(:) Y(:)],mu(:,i)',Sigma2);
 end
 
 % 2) set simulation metadata
-V.T       = 400;                    % # of time steps
-V.dt      = 0.005;                  % time step size
-V.MaxIter = 0;                      % # iterations of EM to estimate params
-V.Np      = Npixs;                  % # of pixels in each image
-V.w       = width;                  % width of frame (pixels)
-V.h       = height;                 % height of frame (pixels)
-V.Nc      = Nc;                     % # cells
-V.plot    = 0;                      % whether to plot filter with each iteration
-V.save    = 0;                                
-V.test      = 0;
+V.T     = 400;                    % # of time steps
+V.dt    = 0.005;                  % time step size
+V.Np    = Npixs;                  % # of pixels in each image
+V.w     = width;                  % width of frame (pixels)
+V.h     = height;                 % height of frame (pixels)
+V.Nc    = Nc;                     % # cells
+V.plot  = 0;                      % whether to plot filter with each iteration
+V.save  = 0;
+V.name  = 'spatial_background';
 
 % 3) initialize params
 for i=1:V.Nc
-    P.a(:,i)=g1(:,i)-1.1*g2(:,i);
+    P.a(:,i)=g1(:,i)-g2(:,i);
 end
 % figure(2), imagesc(reshape(P.a,V.w,V.h)), colormap(gray), colorbar
 
-% P.a(56,1)=P.a(56,1)-P.a(56,1)/5;
 P.b     = 0*P.a(:,1);             % baseline is zero
-P.sig   = 0.05;                    % stan dev of noise (indep for each pixel)
+P.sig   = 0.01;                    % stan dev of noise (indep for each pixel)
 C_0     = 0;                        % initial calcium
 tau     = round(100*rand(V.Nc,1))/100+0.05;   % decay time constant for each cell
 P.gam   = 1-V.dt./tau(1:V.Nc);
 P.lam   = 5;                        % rate-ish, ie, lam*dt=# spikes per second
 
-% 3) simulate data
+% 4) simulate data
 n=zeros(V.T,V.Nc);
 C=n;
 for i=1:V.Nc
@@ -74,9 +72,9 @@ end
 Z = 0*n(:,1);
 F = C*P.a' + repmat(P.b',V.T,1) + P.sig*randn(V.T,Npixs);                  % fluorescence
 
-%% 4) other stuff
+%% 5) make movie
 
-MakMov  = 1;
+MakMov  = 0;
 % make movie of raw data
 if MakMov==1
     for i=1:V.T
@@ -85,122 +83,92 @@ if MakMov==1
     end
 end
 
-% GetROI  = 0;
-% if GetROI
-%     figure(100); clf,imagesc(reshape(sum(g1-g2,2),width,height))
-%     for i=1:Nc
-%         [x y]   = ginput(4);
-%         ROWS    = [round(mean(y(1:2))) round(mean(y(3:4)))];                              % define ROI
-%         COLS    = [round(mean(x([1 4]))) round(mean(x(2:3)))];
-%         COLS1{i}=COLS;
-%         ROWS1{i}=ROWS;
-%         save('ROIs','ROWS1','COLS1')
-%     end
-% else
-%     load('../../data/spatial_background_ROIs.mat')
-% end
+%% 6) infer spike train using various approaches
 
-
-%% end-1) infer spike train using various approaches
-qs=1:2;%:6;%[1 2 3];
-MaxIter=10;
+qs=1:2;
 for q=qs
     GG=F; Tim=V;
     if q==1,
         Phat{q}=P;
-        fast{q}.label='True filter';
+        fast{q}.label='true filter';
     elseif q==2
         Phat{q}=P;
-        fast{q}.label='Estimated filter';
+        boxcar=zeros(V.w,V.h);
+        boxcar(6:10,6:10)=1;
+        Phat{q}.a=boxcar(:);
+        fast{q}.label='boxcar filter';
+    elseif q==3
+        Phat{q}=P;
+        Phat{q}.a=mean(F)';
+        fast{q}.label='mean filter';
     end
     display(fast{q}.label)
     starttime=cputime;
     [fast{q}.n fast{q}.P fast{q}.V] = fast_oopsi(GG',Tim,Phat{q});
     fast{q}.V.time = cputime-starttime;
 end
-if V.save==1, save('../../data/spatial_background'); end
-%% end) plot results
-% load('../../data/spatial2')
-clear Pl
-nrows   = 3+Nc;                                 % set number of rows
+if V.save==1, save(['../../data/' V.name]); end
+
+%% plot results
+% load(['../../data/' V.name])
+
+nrows   = 3;                                 % set number of rows
 ncols   = 2;
 h       = zeros(nrows,1);
-Pl.xlims= [5 V.T-5];                            % time steps to plot
-Pl.nticks=5;                                    % number of ticks along x-axis
-Pl.n    = double(n); Pl.n(Pl.n==0)=NaN;         % store spike train for plotting
-Pl      = PlotParams(Pl);                       % generate a number of other parameters for plotting
-Pl.fs   = 13;
-Pl.vs   = 5;
-Pl.colors(1,:) = [0 0 0];
-Pl.colors(2,:) = Pl.gray;
-Pl.colors(3,:) = [.5 0 0];
-Pl.Nc   = V.Nc;
-Pl.XTicks=100:100:400;
+xlims= [5 V.T-5];                            % time steps to plot
+nticks=5;                                    % number of ticks along x-axis
+fs      = 14;                       % font size
+ms      = 5;                        % marker size for real spike
+sw      = 2;                        % spike width
+lw      = 2;                        % line width
+Nc   = V.Nc;
+XTicks=100:100:400;
+    gray    = 0.4*[1 1 1];            % define gray color
 
-% movie slices
 fig = figure(1); clf,
 
-Nframes=length(Pl.XTicks);
-
-for i=1:Nframes
-    subplot('Position',[0.132+(i-1)*0.21 0.6 .2 .28])
-    if i==1
-        imagesc(reshape(P.a,V.w,V.h))
-        colormap('gray')
-        title([{'true filter'}],'FontSize',Pl.fs)
-    elseif i==3 
-        imagesc(reshape(Phat{2}.a,V.w,V.h))
-        title([{'boxcar filter'}],'FontSize',Pl.fs)
-        set(gca,'YTick',[],'XTick',[])
-    elseif i==4 
-        imagesc(reshape(mean(F),V.w,V.h))
-        title('mean frame','FontSize',Pl.fs)
-        set(gca,'YTick',[],'XTick',[])
-    elseif i==2
-        imagesc(reshape(F(100,:),V.w,V.h))
-        title([{'example frame'}],'FontSize',Pl.fs)
-        set(gca,'YTick',[],'XTick',[])
-    end
-end
+ntemp=double(n);
+ntemp(ntemp==0)=NaN;
 
 for q=qs
-    i=q+3;
-    if q==1, p=2; else p=1; end
-    % plot fluorescence data
-    i=i+1; h(i) = subplot(nrows,ncols,i);
-    if q==1,
-        title(fast{q}.label,'FontSize',Pl.fs+2)
-        Pl.label = 'fluorescence';
-    else
-        Pl.label=[];
-        Pl.interp = 'none';
-        title(fast{q}.label,'FontSize',Pl.fs+2);
-    end
-    Pl.color = 'k';
-    Plot_nX(Pl,(Phat{p}.a\F')');
-
-    % plot inferred spike trains
-    if q==1, 
-        Pl.label = [{'fast'}; {'filter'}]; 
-        Pl.interp = 'none';
-    else
-        Pl.label=[]; 
-    end
-    i=i+2; h(i) = subplot(nrows,ncols,i);
-    Pl.col(2,:)=[0 0 0];
-    Pl.gray=[.5 .5 .5];
-    hold on
-    Plot_n_MAP(Pl,fast{p}.n);
-
-    % set xlabel stuff
+    if q==1, i=0; else i=1; end
+    i=i+1;
     subplot(nrows,ncols,i)
-    set(gca,'XTick',Pl.XTicks,'XTickLabel',Pl.XTicks*V.dt,'FontSize',Pl.fs)
-    xlabel('time (sec)','FontSize',Pl.fs)
+    imagesc(reshape(Phat{q}.a,V.w,V.h))
+    colormap('gray')
+    title(fast{q}.label,'FontSize',fs)
+    
+    i=i+2;
+    subplot(nrows,ncols,i)
+    plot(z1(Phat{q}.a\F'),'k','LineWidth',lw)
+    hold on
+    stem(ntemp,'Marker','+','MarkerSize',ms,'LineStyle','none','MarkerFaceColor',gray,'MarkerEdgeColor',gray)
+
+    set(gca,'XTick',XTicks,'XTickLabel',[],'FontSize',fs)
+    set(gca,'YTick',[],'FontSize',fs)
+    axis([0 V.T 0 1])
+    if q==1, ylabel('fluorescence','FontSize',fs); end
+    box off
+
+    i=i+2;
+    subplot(nrows,ncols,i)
+    bar(fast{q}.n)
+    hold on
+    stem(ntemp,'Marker','+','MarkerSize',ms,'LineStyle','none','MarkerFaceColor',gray,'MarkerEdgeColor',gray)
+    axis('tight')
+    set(gca,'XTick',XTicks,'XTickLabel',XTicks*V.dt,'FontSize',fs)
+    set(gca,'YTick',[],'FontSize',fs)
+    xlabel('time (sec)','FontSize',fs)
+    if q==1, ylabel('fast filter','FontSize',fs); end
+    box off
 end
+
 
 if V.save==1 % print fig
     wh=[7 5];   %width and height
-    DirName = '../../figs/';
-    FileName = 'spatial_background';
-    PrintFig(wh,DirName,FileName);
+    set(gcf,'PaperSize',wh,'PaperPosition',[0 0 wh],'Color','w');
+    figname=;'../../figs/' V.name];
+    print('-depsc',figname)
+    print('-dpdf',figname)
+    saveas(fig,figname)
 end
