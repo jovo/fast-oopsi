1275530c2092fb1594b4a5c09d448b70a5474d0d
jovo <joshuav@jhu.edu>
2010-02-21 13:39:17 -0500

    updated fig 2

--- code/scripts/MakeFigScripts.m
+++ code/scripts/MakeFigScripts.m
@@ -2,7 +2,19 @@
 
 V.datasets  = 13;
 V.filters   = [1 4];
-V.name      = 'fast_wiener_sim';
+V.name      = 'fast_wiener_inf';
+V.save      = 1;
+
+P.lam       = 1;
+P.sig       = 0.2;
+
+MakeFigs(V,P);
+
+%% make figure 4
+
+V.datasets  = 13;
+V.filters   = [1 4];
+V.name      = 'fast_wiener_learn';
 V.save      = 1;
 
 P.lam       = 1;
